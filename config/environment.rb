ad9350297b2f28726852c6707295d0c3ec50d42c
aurel fontaine <ajfame11@gmail.com>
2020-06-08 23:49:45 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,5 @@ Bundler.require
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/costumes.sqlite")
+ActiveRecord::Base.establish_connection(connection_details)
   
-  require_relative "../costume.rb"

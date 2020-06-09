03b757a02f32a42950df44ca4c2e958cbce5023b
aurel fontaine <ajfame11@gmail.com>
2020-06-09 00:00:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.string :price
+      t.string :location
       t.integer :size
       t.string :image_url
       t.timestamps

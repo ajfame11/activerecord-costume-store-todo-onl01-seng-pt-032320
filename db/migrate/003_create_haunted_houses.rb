d1fb3fcc1b5d4e204265f089570947fa0bf5a302
aurel fontaine <ajfame11@gmail.com>
2020-06-09 00:22:13 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,10 +3,10 @@ class HauntedHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name
-      t.string :price
-      t.integer :size
-      t.string :image_url
-      t.timestamps
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :Family_friendly
     end
   end
 end
